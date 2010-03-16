bb3b92d537358b50286d57ce3ea88030b0bd9175
cornelius <cornelius@65ff189f-35ee-0310-953c-23049635c3cf>
2010-03-16 03:08:17 +0000

    zojax.django.contentitem: initial upload

---
+++ src/zojax/django/contentitem/models.py
@@ -0,0 +1,39 @@
+from autoslug.fields import AutoSlugField
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+import datetime
+
+
+class ContentItemManager(models.Manager):
+    
+    def published(self):
+        return super(ContentItemManager, self).get_query_set().filter(published=True) 
+
+
+class ContentItem(models.Model):
+    
+    title = models.CharField(max_length=150)
+    slug = AutoSlugField(populate_from='title', verbose_name=_(u"Slug"), always_update=True)
+
+    published = models.BooleanField(default=False)
+    published_on = models.DateTimeField(null=True, blank=True)
+
+    created_on = models.DateTimeField(default=datetime.datetime.now)
+
+    objects = ContentItemManager()
+
+    def __unicode(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        super(ContentItem, self).save(*args, **kwargs)
+        if self.published and not self.published_on:
+            self.published_on = datetime.datetime.now()
+            
+        if not self.published:
+            self.published_on = None
+
+    class Meta:
+        abstract = True
+        
